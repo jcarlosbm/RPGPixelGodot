8fd804dd75b63ea5b6e2aa74a240b57e834294c8
Juan Carlos Bonilla <bonillasoftdev@gmail.com>
2024-04-05 21:18:34 -0400

    First commit

---
+++ Player/Player.cs
@@ -0,0 +1,36 @@
+using System;
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+	//private int speed = 3;
+	private const int MAXSPEED = 100;
+	private const int ACCELERATION = 100;
+	private const int FRICTION = 400; 
+	Vector2 velocity = new Vector2(0,0);
+
+    public override void _PhysicsProcess(double delta)
+    {
+
+        Vector2 inputVector = new Vector2(0, 0)
+        {
+            X = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"),
+            Y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up")
+        };
+		inputVector = inputVector.Normalized();
+
+		if (inputVector != Vector2.Zero){
+			
+			velocity = velocity.MoveToward(inputVector * MAXSPEED, ACCELERATION * (float)delta);
+
+		}else{
+			//velocity = Vector2.Zero;
+			velocity = velocity.MoveToward(Vector2.Zero, FRICTION * (float)delta);
+		}
+		
+        MoveAndCollide(velocity * (float)delta);
+
+	}
+}
+
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# RPGPixelGodot
+
+Este repositorio sigue los tutoriales de la serie "Godot Action RPG Series" del canal Heartbeast en YouTube.
+
+## Descripción
+Este repositorio contiene el código fuente y los recursos utilizados para seguir los tutoriales de Heartbeast sobre cómo crear un RPG de acción con Godot utilizando C#. El proyecto se basa en el motor de juegos Godot y se desarrolla en el entorno de desarrollo integrado (IDE) VS Code.
+
+## Serie de Tutoriales
+Puedes encontrar la serie de tutoriales aquí: [Godot Action RPG Series](https://youtube.com/playlist?list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&si=QQPlw_S_Uxij04yW)
+
+## Versiones Utilizadas
+- Godot: 4.2.1 estable.mono
+- DotNet: 8.0.203
+- VS Code: 1.87.2 (Universal)
