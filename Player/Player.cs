a0f31629e527330a778076e481d0b938567a981f
Juan Carlos Bonilla <bonillasoftdev@gmail.com>
2024-04-05 22:46:17 -0400

    Replaced MoveAndCollide with MoveAndSlide

--- Player/Player.cs
+++ Player/Player.cs
@@ -5,7 +5,7 @@ public partial class Player : CharacterBody2D
 {
 	//private int speed = 3;
 	private const int MAXSPEED = 100;
-	private const int ACCELERATION = 100;
+	private const int ACCELERATION = 400;
 	private const int FRICTION = 400; 
 	Vector2 velocity = new Vector2(0,0);
 
@@ -22,14 +22,17 @@ public partial class Player : CharacterBody2D
 		if (inputVector != Vector2.Zero){
 			
 			velocity = velocity.MoveToward(inputVector * MAXSPEED, ACCELERATION * (float)delta);
+			Velocity = velocity;
 
 		}else{
 			//velocity = Vector2.Zero;
 			velocity = velocity.MoveToward(Vector2.Zero, FRICTION * (float)delta);
-		}
-		
-        MoveAndCollide(velocity * (float)delta);
+			Velocity = velocity;
 
+		}
+
+		 MoveAndSlide();
+    
 	}
 }
 
