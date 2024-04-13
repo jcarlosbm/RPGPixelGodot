5858057f8b27e65b77c23a864f474df9d752b689
Juan Carlos Bonilla <bonillasoftdev@gmail.com>
2024-04-13 13:11:02 -0400

    Update Progres: Video #9. Changes: - Added State Machines to Player - Added Roll and Attack animations

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,8 +1,16 @@
 using System;
 using Godot;
 
+
 public partial class Player : CharacterBody2D
 {
+
+	enum State{
+		MOVE,
+		ROLL,
+		ATTACk
+	} 
+
 	//private int speed = 3;
 	private const int MAXSPEED = 70;
 	private const int ACCELERATION = 400;
@@ -11,11 +19,17 @@ public partial class Player : CharacterBody2D
 	private AnimationPlayer animationPlayer;
 	private AnimationTree animationTree;
 	private AnimationNodeStateMachinePlayback animationNodeStateMachinePlayback;
+	private State state = State.MOVE;
+	
 
+	
 
     public override void _Ready()
     {
         base._Ready();
+
+		//animationTree.Active = true;
+
 		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		animationTree = GetNode<AnimationTree>("AnimationTree");
 		animationNodeStateMachinePlayback = (AnimationNodeStateMachinePlayback) animationTree.Get("parameters/playback");
@@ -24,8 +38,20 @@ public partial class Player : CharacterBody2D
     }
     public override void _PhysicsProcess(double delta)
     {
+		switch (state){
+			case State.MOVE: moveSate(delta);
+			break;
+			case State.ATTACk: attackState(delta);
+			break;
+			case State.ROLL: rollState(delta);
+			break;
+		}
 
-        Vector2 inputVector = new Vector2(0, 0)
+    
+	}
+
+	private void moveSate(double delta){
+		Vector2 inputVector = new Vector2(0, 0)
         {
             X = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"),
             Y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up")
@@ -36,6 +62,8 @@ public partial class Player : CharacterBody2D
 
 			animationTree.Set("parameters/Idle/blend_position", inputVector);
 			animationTree.Set("parameters/Run/blend_position", inputVector);
+			animationTree.Set("parameters/Attack/blend_position", inputVector);
+
 
 			animationNodeStateMachinePlayback.Travel("Run");
 			velocity = velocity.MoveToward(inputVector * MAXSPEED, ACCELERATION * (float)delta);
@@ -49,9 +77,25 @@ public partial class Player : CharacterBody2D
 
 		}
 
+		if(Input.IsActionJustPressed("Attack")){
+			state = State.ATTACk;
+		}
+
 		 MoveAndSlide();
-    
+	}
+
+	private void rollState(double delta){
+
+	}
+
+	private void attackState(double delta){
+		 animationNodeStateMachinePlayback.Travel("Attack");
+	}
+
+	private void attackAnimationFinished(){
+		state = State.MOVE;
 	}
 }
+
 
 
